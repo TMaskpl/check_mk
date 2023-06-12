5a08f0305c0ee13f2217c5297b4076140368835b
TMaskpl <75216446+TMaskpl@users.noreply.github.com>
2023-06-12 10:53:43 +0200

    Create Check_windows_process.ps1

---
+++ Check_windows_process.ps1
@@ -0,0 +1,27 @@
+$FIRMA = "JUSTMAR"
+$MAIL = "biuro@tmask.pl"
+$SEM = "services.sem"
+# $SLACK = "https://hooks.slack.com/services/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"
+$SLACKCH = "softera"
+$D = $(Get-Date -Format "dddd MM/dd/yyyy HH:mm")
+
+
+$list = get-content "services.txt"
+
+foreach ($service in $list) {
+
+if ((Get-Service -Name $service).Status -EQ "Running") {
+
+    Write-Host "0 ""Check Production Service"" - OK Service $service running"
+}
+
+else {
+
+    Write-Host "2 ""Check Production Service"" - ERROR Service $service not running"
+    # curl -X POST --data-urlencode "payload={\"channel\": \"#$SLACKCH\", \"username\": \"$FIRMA \", \"text\": \"ERROR service - $s \", \"icon_emoji\": \":red_circle:\"}" $SLACK
+    Write-EventLog -LogName "Application" -Source "TMaskPL" -EventID 3001 -EntryType Error -Message "$D - ERROR Service $service not running" -Category 1 -RawData 10,20
+    exit 1
+
+    }
+
+}
